87e979c9326d48ef59975496f7687f1fc5ebeaa4
Saad Tbili <saadtbilioz@gmail.com>
2026-08-07 23:00:07 +0400

    clean_up_scratch_directory

--- scratch/countTools.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/data/saasData.jsx', 'utf8');
-const domainMatches = content.match(/"domain":/g);
-console.log('TOTAL_TOOLS_COUNT:', domainMatches ? domainMatches.length : 0);
